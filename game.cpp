4d5e9d911697b3305ce7ef23e9b4f4f9b1319d2f
Flumming <flemming.vandersande@gmail.com>
2023-02-13 17:22:08 +0100

    Changes

--- game.cpp
+++ game.cpp
@@ -5,22 +5,30 @@
 #include <cstdio> //printf
 #include "LDtkLoader/Project.hpp"
 #include "collision.h"
+#include <typeinfo>
 
 namespace Tmpl8
 {
+	class Collision {
+		public: 
+			int collisions;
+	};
+
 	// -----------------------------------------------------------
 	// Initialize the application
 	// -----------------------------------------------------------
 	void Game::Init()
 	{
+		Collision collision;
 		ldtk::Project ldtk_project;
 		ldtk_project.loadFromFile("map/ldtk/testmap.ldtk");
 		const auto& world = ldtk_project.getWorld("");
 		const auto& level1 = world.getLevel("Level_0");
 		const auto& entities = level1.getLayer("Entities");
 		const auto& collisions = entities.getEntitiesByName("Collision");
+		collision.collisions = collisions;
 	}
-	
+
 	// -----------------------------------------------------------
 	// Close down application
 	// -----------------------------------------------------------
@@ -69,34 +77,40 @@ namespace Tmpl8
 		int playercolor = (234<<16) + (255);
 		screen->DrawCircle(cx, cy, playerr/2, playercolor);
 		//Bounding box level
-		
-		for (auto& collision : collisions)
-		{
-			auto& e = collision.get();
-			auto& pos = e.getPosition();
-			auto& size = e.getSize();
-			auto& sticky = e.getField<ldtk::FieldType::Bool>("Sticky");
-			auto& isSticky = sticky.value();
-			int color = 0;
-			if (isSticky == 1)
-			{
-				color = (255 << 16) + (153 << 8);
-			}
-			else
-			{
-				color = (255 << 16);
-			}
-			int min_x = ((pos.x * scale) - camerax);
-			int min_y = ((pos.y * scale - offset) + cameray);
-			int max_x = (((pos.x + size.x) * scale) - camerax);
-			int max_y = (((pos.y + size.y) * scale - offset) + cameray);
-			bool hasCollision = CollisionCircleAABB(playerx, playery, playerr, min_x, min_y, max_x, max_y);
-			if (hasCollision)
-			{
-				color = (34 << 16) + (255 << 8);
-			}
-			screen->Box(min_x, min_y, max_x, max_y, color);
-		}
+		ldtk::Project ldtk_project;
+		ldtk_project.loadFromFile("map/ldtk/testmap.ldtk");
+		const auto& world = ldtk_project.getWorld("");
+		const auto& level1 = world.getLevel("Level_0");
+		const auto& entities = level1.getLayer("Entities");
+		const auto& collisions = entities.getEntitiesByName("Collision");
+		printf("%i\n", typeid(&collisions).name());
+		//for (auto& collision : collisions)
+		//{
+		//	auto& e = collision.get();
+		//	auto& pos = e.getPosition();
+		//	auto& size = e.getSize();
+		//	auto& sticky = e.getField<ldtk::FieldType::Bool>("Sticky");
+		//	auto& isSticky = sticky.value();
+		//	int color = 0;
+		//	if (isSticky == 1)
+		//	{
+		//		color = (255 << 16) + (153 << 8);
+		//	}
+		//	else
+		//	{
+		//		color = (255 << 16);
+		//	}
+		//	int min_x = ((pos.x * scale) - camerax);
+		//	int min_y = ((pos.y * scale - offset) + cameray);
+		//	int max_x = (((pos.x + size.x) * scale) - camerax);
+		//	int max_y = (((pos.y + size.y) * scale - offset) + cameray);
+		//	bool hasCollision = CollisionCircleAABB(playerx, playery, playerr, min_x, min_y, max_x, max_y);
+		//	if (hasCollision)
+		//	{
+		//		color = (34 << 16) + (255 << 8);
+		//	}
+		//	screen->Box(min_x, min_y, max_x, max_y, color);
+		//}
 		player.DrawScaled(playerx, playery, (8 * scale), (8 * scale), screen);
 		
 		printf("");
